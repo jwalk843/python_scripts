133346a147ad7238fbc6b4ef6d92eac5df5713c3
J <jw84323@gmail.com>
2020-09-15 16:33:36 -0400

    Update ctf_client.py

--- ctf_client.py
+++ ctf_client.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
 import socket
-# gets response from server and prints it out
+import sys
 
-SOCKET = None
-MAXBUF = 4096
+class ConnectClass():
+    """
+    My custom client server
+    """
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
-def get():
-    data = SOCKET.recv(MAXBUF)
-    return data.decode()
+        # Establish the connection to the target:port
+        print(f"Connecting to {self.host}:{self.port}...")
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.s.connect((self.host, self.port))
+        except OSError as e:
+            print(e)
 
-def send(data):
-    SOCKET.sendall(data.encode())
-
-SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-SOCKET.connect(("challenges.ctfd.io", 30070))
-
-data_in = get()
-print(data_in)
-send("100")
-data_in = get()
-print(data_in)
-
+    def recv_data(self, maxbuffer=4096):
+        """
+        Receive the data from the server
+        """
+        try:
+            buff = self.s.recv(maxbuffer)
+        except OSError as e:
+            print(e)
+        return buff.decode()
+        
+    def send_data(self, str_data):
+        """
+        Send the data to the server
+        """
+        try:
+            self.s.sendall(str_data.encode())
+        except OSError as e:
+            print(e)
